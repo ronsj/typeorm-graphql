17682950699f7537db9b507dc8f4baa4ae2ac2c2
Ron San Jose <ronsj1@gmail.com>
2022-02-09 21:35:59 -0800

    remove comments

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,12 +1,7 @@
 import { getRepository } from 'typeorm'
-
-// import { Recipe } from "../entities/recipe";
-// import { Rate } from "../entities/rate";
 import { User } from './entity/User'
 
 export async function seedDatabase() {
-  // const recipeRepository = getRepository(Recipe);
-  // const ratingsRepository = getRepository(Rate);
   const userRepository = getRepository(User)
 
   const defaultUser = userRepository.create({
@@ -29,30 +24,6 @@ export async function seedDatabase() {
     },
   ])
   await userRepository.save(users)
-
-  // const recipes = recipeRepository.create([
-  //   {
-  //     title: "Recipe 1",
-  //     description: "Desc 1",
-  //     author: defaultUser,
-  //     ratings: ratingsRepository.create([
-  //       { value: 2, user: defaultUser },
-  //       { value: 4, user: defaultUser },
-  //       { value: 5, user: defaultUser },
-  //       { value: 3, user: defaultUser },
-  //       { value: 4, user: defaultUser },
-  //     ]),
-  //   },
-  //   {
-  //     title: "Recipe 2",
-  //     author: defaultUser,
-  //     ratings: ratingsRepository.create([
-  //       { value: 2, user: defaultUser },
-  //       { value: 4, user: defaultUser },
-  //     ]),
-  //   },
-  // ]);
-  // await recipeRepository.save(recipes);
 
   return {
     defaultUser,
